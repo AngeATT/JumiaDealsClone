924e96fd7aee3ee3dfd87d417941a4b335da5c65
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-07-10 17:18:49 +0000

    feat : renvoie d'un json pour l'erreur d'authentification

---
+++ src/main/java/ci/jumia/deals/security/jwt/AuthEntryPointJwt.java
@@ -0,0 +1,41 @@
+package ci.jumia.deals.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+/**
+ * <p></p>
+ *
+ * @author Attoungbre Ange François 2023-07-10
+ */
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+  public static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response,
+      AuthenticationException authException) throws IOException {
+    logger.error("Erreur ressource non autorisée : {}", authException.getMessage());
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+    final Map<String, Object> body = new HashMap<>();
+    body.put("statut",HttpServletResponse.SC_UNAUTHORIZED);
+    body.put("erreur","Non autorisé");
+    body.put("message :", authException.getMessage());
+    body.put("path",request.getServletPath());
+
+    final ObjectMapper mapper = new ObjectMapper();
+    mapper.writeValue(response.getOutputStream(), body);
+  }
+}
